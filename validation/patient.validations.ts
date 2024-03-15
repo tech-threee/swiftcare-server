ed0d50a0e873aafab78b0ec27cf153deabf7730c
qbentil <bentilshadrack72@gmail.com>
2024-03-15 16:10:40 +0000

    Add patient create validations

---
+++ validation/patient.validations.ts
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+const PatientValidations = {
+    create: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        phone: Joi.string().required(),
+        dob: Joi.string().required(),
+        pid: Joi.string().optional(),
+        emergency_contacts: Joi.array().optional()
+    }),
+    readOne: Joi.object().keys({
+        communicationId: Joi.string().required(),
+    }),
+    reply: Joi.object().keys({
+        text: Joi.string().required(),
+    }),
+};
+
+export default PatientValidations;
