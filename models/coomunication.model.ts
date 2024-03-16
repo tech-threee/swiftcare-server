4ad6fcc7ecd3528bad20d1f70cd7e5d9bd52defb
qbentil <bentilshadrack72@gmail.com>
2024-03-16 10:35:37 +0000

    Add communication model

---
+++ models/coomunication.model.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+import { Communication } from '../interfaces/communication.interface';
+import AppConstants from '../constants/app.constant';
+
+const communicationSchema = new mongoose.Schema<Communication>(
+    {
+        sender: {
+            participantId: mongoose.Schema.Types.ObjectId,
+            role: {
+                enum: Object.values(AppConstants.MODULES)
+            },
+            email: String,
+        },
+        recipients: [
+            {
+                participantId: mongoose.Schema.Types.ObjectId,
+                role: {
+                    enum: Object.values(AppConstants.MODULES)
+                },
+                email: String,
+            },
+        ],
+        text: String,
+        replies: [
+            {
+                sender: {
+                    participantId: mongoose.Schema.Types.ObjectId,
+                    role: {
+                        enum: Object.values(AppConstants.MODULES)
+                    },
+                    email: String,
+                },
+                text: String,
+            },
+        ],
+    },
+    { timestamps: true, versionKey: false },
+);
+
+export default mongoose.models['COMMUNICATION'] ||
+    mongoose.model('COMMUNICATION', communicationSchema);
